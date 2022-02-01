b4d849c0944ca6b12d382fb5ce1514e838dde3a0
Cole Sulak <nicole12sulak@gmail.com>
2022-02-01 10:32:35 -0600

    Adding variables to the js file for Form Validation

---
+++ javascript/JavaScript-Lab Resources/Form Validation/javascript.js
@@ -0,0 +1,8 @@
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+if (firstName.value === '' || firstName.value === null){
+  prompt('First Name cannot be empty')
+  }
