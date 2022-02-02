366b3934c2ea6e1a698919e38edc9c9e76a91df0
Cole Sulak <nicole12sulak@gmail.com>
2022-02-02 13:04:17 -0600

    Changed first name textbox to show the error message and red outline, and made initial text prompt disappear after text entry is made

--- javascript/JavaScript-Lab Resources/Form Validation/javascript.js
+++ javascript/JavaScript-Lab Resources/Form Validation/javascript.js
@@ -8,7 +8,18 @@ const errorFirstName = document.getElementById('first-name-error');
 button.addEventListener('click', function () {
 
 if (firstName.value === '' || firstName.value === null){
+    
+  if (firstName.value === '' || firstName.value === null) {
+    errorFirstName.style.visibility = 'visible';
+    firstName.classList.add('error');
+    firstName.placeholder = '';
+  } else {
+    errorFirstName.style.visibility = 'hidden';
+    firstName.classList.remove('error');
+  }
   
+
+
   }
 
 })
