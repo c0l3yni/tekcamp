55191cb1a833c5f2125c932ad66968a96f6e902b
Cole Sulak <nicole12sulak@gmail.com>
2022-02-02 12:53:17 -0600

    Adding variables to javascript file and creating the click event listener for first name errors

--- javascript/JavaScript-Lab Resources/Form Validation/javascript.js
+++ javascript/JavaScript-Lab Resources/Form Validation/javascript.js
@@ -2,7 +2,13 @@ const firstName = document.getElementById('first-name');
 const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const button = document.getElementById('form-submit');
+const errorFirstName = document.getElementById('first-name-error');
+
+button.addEventListener('click', function () {
 
 if (firstName.value === '' || firstName.value === null){
-  prompt('First Name cannot be empty')
+  
   }
+
+})
