647600490bbcda50fd70a6cb6192d46753bc0788
Cole Sulak <nicole12sulak@gmail.com>
2022-02-02 14:42:27 -0600

    Added error image to first name text box and defined last name, email, and password variables in java script. Moved Javascript src to bottom of html doc

--- javascript/JavaScript-Lab Resources/Form Validation/javascript.js
+++ javascript/JavaScript-Lab Resources/Form Validation/javascript.js
@@ -4,22 +4,23 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const button = document.getElementById('form-submit');
 const errorFirstName = document.getElementById('first-name-error');
+const errorLastName = document.getElementById('last-name-error');
+const errorEmail = document.getElementById('email-error');
+const errorPassword = document.getElementById('password-error');
+const errorImage = document.getElementsByClassName('fas fa-exclamation-circle');
 
-button.addEventListener('click', function () {
+button.addEventListener('click', function(){
 
-if (firstName.value === '' || firstName.value === null){
-    
-  if (firstName.value === '' || firstName.value === null) {
-    errorFirstName.style.visibility = 'visible';
-    firstName.classList.add('error');
-    firstName.placeholder = '';
-  } else {
-    errorFirstName.style.visibility = 'hidden';
-    firstName.classList.remove('error');
-  }
-  
+    if (firstName.value === '' || firstName.value === null) {
+      errorFirstName.style.visibility = 'visible';
+      firstName.classList.add('error');
+      firstName.placeholder = '';
+      errorImage[0].style.visibility = 'visible';
+    } else {
+      errorFirstName.style.visibility = 'hidden';
+      firstName.classList.remove('error');
+      errorImage[0].style.visibility = 'hidden';
+    }
 
-
-  }
-
-})
+   
+  });
