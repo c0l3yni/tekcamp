935c339957d8b6aab148c99d726df78d0066a755
Cole Sulak <nicole12sulak@gmail.com>
2022-02-02 15:11:39 -0600

    Added same error effects to email  that first name variable had and added the regex email verification

---
+++ javascript/JavaScript-Lab Resources/Form Validation/README-challenges.md
@@ -0,0 +1,1 @@
+Regex from https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript
--- javascript/JavaScript-Lab Resources/Form Validation/javascript.js
+++ javascript/JavaScript-Lab Resources/Form Validation/javascript.js
@@ -8,6 +8,7 @@ const errorLastName = document.getElementById('last-name-error');
 const errorEmail = document.getElementById('email-error');
 const errorPassword = document.getElementById('password-error');
 const errorImage = document.getElementsByClassName('fas fa-exclamation-circle');
+const approvedEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 button.addEventListener('click', function(){
 
@@ -33,6 +34,17 @@ button.addEventListener('click', function(){
       errorImage[1].style.visibility = 'hidden';
     }
 
+    if (email.value.match(approvedEmail)) {
+      errorEmail.style.visibility = 'hidden';
+      email.classList.remove('error');
+      errorImage[2].style.visibility = 'hidden';
+    } else {
+      errorEmail.style.visibility = 'visible';
+      email.classList.add('error');
+      email.placeholder = '';
+      errorImage[2].style.visibility = 'visible';
+    }
+
     if (password.value === '' || password.value === null) {
       errorPassword.style.visibility = 'visible';
       password.classList.add('error');
@@ -43,4 +55,4 @@ button.addEventListener('click', function(){
       password.classList.remove('error');
       errorImage[3].style.visibility = 'hidden';
     }    
-  });
+  })
