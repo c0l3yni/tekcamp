64dd80f6484cb2d99bf52e20b574cf58fb088f9b
Cole Sulak <nicole12sulak@gmail.com>
2022-02-02 21:03:11 -0600

    Added spacing between lines of code in javascript

--- javascript/JavaScript-Lab Resources/Form Validation/javascript.js
+++ javascript/JavaScript-Lab Resources/Form Validation/javascript.js
@@ -13,58 +13,58 @@ const passwordErrorText = document.getElementById('id')
 
 button.addEventListener('click', function(){
 
-    if (firstName.value === '' || firstName.value === null) {
-      errorFirstName.style.visibility = 'visible';
-      errorFirstName.style.fontStyle = 'italic';
-      firstName.classList.add('error');
-      firstName.placeholder = '';
-      errorImage[0].style.visibility = 'visible';
-    } else {
-      errorFirstName.style.visibility = 'hidden';
-      firstName.classList.remove('error');
-      errorImage[0].style.visibility = 'hidden';
-    }
+  if (firstName.value === '' || firstName.value === null) {
+    errorFirstName.style.visibility = 'visible';
+    errorFirstName.style.fontStyle = 'italic';
+    firstName.classList.add('error');
+    firstName.placeholder = '';
+    errorImage[0].style.visibility = 'visible';
+  } else {
+    errorFirstName.style.visibility = 'hidden';
+    firstName.classList.remove('error');
+    errorImage[0].style.visibility = 'hidden';
+  }
 
-    if (lastName.value === '' || lastName.value === null) {
-      errorLastName.style.visibility = 'visible';
-      errorLastName.style.fontStyle = 'italic';
-      lastName.classList.add('error');
-      lastName.placeholder = '';
-      errorImage[1].style.visibility = 'visible';
-    } else {
-      errorLastName.style.visibility = 'hidden';
-      lastName.classList.remove('error');
-      errorImage[1].style.visibility = 'hidden';
-    }
+  if (lastName.value === '' || lastName.value === null) {
+    errorLastName.style.visibility = 'visible';
+    errorLastName.style.fontStyle = 'italic';
+    lastName.classList.add('error');
+    lastName.placeholder = '';
+    errorImage[1].style.visibility = 'visible';
+  } else {
+    errorLastName.style.visibility = 'hidden';
+    lastName.classList.remove('error');
+    errorImage[1].style.visibility = 'hidden';
+  }
 
-    if (email.value === '' || email.value === null) {
-      blankEmail.style.visibility = 'visible';
-      blankEmail.style.fontStyle = 'italic';
-      email.classList.add('error');
-      email.placeholder = '';
-      errorImage[2].style.visibility = 'visible';
-    } else if (email.value.match(approvedEmail)) {
-      blankEmail.style.visibility = 'hidden';
-      email.classList.remove('error');
-      errorImage[2].style.visibility = 'hidden';
-    } else {
-      blankEmail.innerText = 'Looks like this is not an email';
-      blankEmail.style.visibility = 'visible';
-      blankEmail.style.fontStyle = 'italic';
-      email.classList.add('error');
-      email.placeholder = '';
-      errorImage[2].style.visibility = 'visible';
-    }
+  if (email.value === '' || email.value === null) {
+    blankEmail.style.visibility = 'visible';
+    blankEmail.style.fontStyle = 'italic';
+    email.classList.add('error');
+    email.placeholder = '';
+    errorImage[2].style.visibility = 'visible';
+  } else if (email.value.match(approvedEmail)) {
+    blankEmail.style.visibility = 'hidden';
+    email.classList.remove('error');
+    errorImage[2].style.visibility = 'hidden';
+  } else {
+    blankEmail.innerText = 'Looks like this is not an email';
+    blankEmail.style.visibility = 'visible';
+    blankEmail.style.fontStyle = 'italic';
+    email.classList.add('error');
+    email.placeholder = '';
+    errorImage[2].style.visibility = 'visible';
+  }
 
-    if (password.value === '' || password.value === null) {
-      errorPassword.style.visibility = 'visible';
-      errorPassword.style.fontStyle = 'italic';
-      password.classList.add('error');
-      password.placeholder = '';
-      errorImage[3].style.visibility = 'visible';
-    } else {
-      errorPassword.style.visibility = 'hidden';
-      password.classList.remove('error');
-      errorImage[3].style.visibility = 'hidden';
-    }    
+  if (password.value === '' || password.value === null) {
+    errorPassword.style.visibility = 'visible';
+    errorPassword.style.fontStyle = 'italic';
+    password.classList.add('error');
+    password.placeholder = '';
+    errorImage[3].style.visibility = 'visible';
+  } else {
+    errorPassword.style.visibility = 'hidden';
+    password.classList.remove('error');
+    errorImage[3].style.visibility = 'hidden';
+  }    
   })
