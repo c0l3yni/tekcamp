bc6af197d5bb70f13a37a66d4cff7db095c37e8f
Cole Sulak <nicole12sulak@gmail.com>
2022-02-03 21:43:59 -0600

    Added the open and close functionality to question 2, also added the arrow rotation, the text changing to and from bold

--- javascript/JavaScript-Lab Resources/Accordion/scriptAccordion.js
+++ javascript/JavaScript-Lab Resources/Accordion/scriptAccordion.js
@@ -21,3 +21,27 @@ question[0].addEventListener('click', function() {
     arrowIcon[0].style.transform = 'rotate(0deg)';
   }
 });
+
+question[1].addEventListener('click', function() {
+  question[1].addEventListener('mouseenter', function() {
+    document.body.style.cursor = 'pointer';
+  });
+  question[1].addEventListener('mouseleave', function() {
+    document.body.style.cursor = 'default';
+  });
+  
+  if (answer[1].style.display === 'none') {
+    answer[1].style.display = 'block';
+    question[1].style.fontWeight = 'bold';
+    arrowIcon[1].style.transform = 'rotate(180deg)';
+  } else {
+    answer[1].style.display = 'none';
+    question[1].style.fontWeight = 'normal';
+    arrowIcon[1].style.transform = 'rotate(0deg)';
+  }
+});
+
+
+
+ 
+  
