216ab4f55539e4e281d1dd8d0418e145acf954de
Cole Sulak <nicole12sulak@gmail.com>
2022-02-03 19:01:03 -0600

    Created function to allow accordion to open for question 1 with arrow rotation and cursor to point functionality

--- javascript/JavaScript-Lab Resources/Accordion/scriptAccordion.js
+++ javascript/JavaScript-Lab Resources/Accordion/scriptAccordion.js
@@ -1,1 +1,23 @@
-const 
+const question = document.getElementsByClassName('question');
+const answer = document.getElementsByClassName('answer');
+const arrowIcon = document.getElementsByClassName('arrow-icon')
+
+question[0].addEventListener('mouseenter', function() {
+  document.body.style.cursor = 'pointer';
+
+});
+question[0].addEventListener('mouseleave', function() {
+  document.body.style.cursor = 'default';
+
+});
+
+question[0].addEventListener('click', function() {
+  
+  answer[0].style.display = 'block';
+  arrowIcon[0].style.transform = 'rotate(180deg)';
+
+});
+
+
+
+  
