a5901fb955eb29ea03a33923baf4362d6dbc1562
Cole Sulak <nicole12sulak@gmail.com>
2022-02-03 21:42:23 -0600

    Added accordion open and shut functionality to js file

--- javascript/JavaScript-Lab Resources/Accordion/scriptAccordion.js
+++ javascript/JavaScript-Lab Resources/Accordion/scriptAccordion.js
@@ -1,23 +1,23 @@
-const question = document.getElementsByClassName('question');
+const question = document.getElementsByClassName('faq-item');
 const answer = document.getElementsByClassName('answer');
 const arrowIcon = document.getElementsByClassName('arrow-icon')
 
-question[0].addEventListener('mouseenter', function() {
-  document.body.style.cursor = 'pointer';
-
-});
-question[0].addEventListener('mouseleave', function() {
-  document.body.style.cursor = 'default';
-
-});
 
 question[0].addEventListener('click', function() {
+  question[0].addEventListener('mouseenter', function() {
+    document.body.style.cursor = 'pointer';
+  });
+  question[0].addEventListener('mouseleave', function() {
+    document.body.style.cursor = 'default';
+  });
   
-  answer[0].style.display = 'block';
-  arrowIcon[0].style.transform = 'rotate(180deg)';
-
+  if (answer[0].style.display === 'none') {
+    answer[0].style.display = 'block';
+    question[0].style.fontWeight = 'bold';
+    arrowIcon[0].style.transform = 'rotate(180deg)';
+  } else {
+    answer[0].style.display = 'none';
+    question[0].style.fontWeight = 'normal';
+    arrowIcon[0].style.transform = 'rotate(0deg)';
+  }
 });
-
-
-
-  
