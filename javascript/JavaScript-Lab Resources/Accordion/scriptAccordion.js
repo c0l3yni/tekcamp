2aa8e9c34c7c71e4318278d1248ab51c52be1033
Cole Sulak <nicole12sulak@gmail.com>
2022-02-04 17:31:40 -0600

    Moved the javascript script tag up in the index.html file, and switched my if/else statements to increase click responsiveness

--- javascript/JavaScript-Lab Resources/Accordion/scriptAccordion.js
+++ javascript/JavaScript-Lab Resources/Accordion/scriptAccordion.js
@@ -9,17 +9,16 @@ question[0].addEventListener('mouseleave', function () {
   document.body.style.cursor = 'default';
 });
 question[0].addEventListener('click', function () {
-  if (answer[0].style.display === 'none') {
-    answer[0].style.display = 'block';
-    question[0].style.fontWeight = 'bold';
-    arrowIcon[0].style.transform = 'rotate(180deg)';
-  } else {
+  if (answer[0].style.display === 'block') {
     answer[0].style.display = 'none';
     question[0].style.fontWeight = 'normal';
     arrowIcon[0].style.transform = 'rotate(0deg)';
+  } else {
+    answer[0].style.display = 'block';
+    question[0].style.fontWeight = 'bold';
+    arrowIcon[0].style.transform = 'rotate(180deg)';
   }
 });
-
 
 question[1].addEventListener('mouseenter', function () {
   document.body.style.cursor = 'pointer';
@@ -28,17 +27,16 @@ question[1].addEventListener('mouseleave', function () {
   document.body.style.cursor = 'default';
 });
 question[1].addEventListener('click', function () {
-  if (answer[1].style.display === 'none') {
-    answer[1].style.display = 'block';
-    question[1].style.fontWeight = 'bold';
-    arrowIcon[1].style.transform = 'rotate(180deg)';
-  } else {
+  if (answer[1].style.display === 'block') {
     answer[1].style.display = 'none';
     question[1].style.fontWeight = 'normal';
     arrowIcon[1].style.transform = 'rotate(0deg)';
+  } else {
+    answer[1].style.display = 'block';
+    question[1].style.fontWeight = 'bold';
+    arrowIcon[1].style.transform = 'rotate(180deg)';
   }
 });
-
 
 question[2].addEventListener('mouseenter', function () {
   document.body.style.cursor = 'pointer';
@@ -47,17 +45,16 @@ question[2].addEventListener('mouseleave', function () {
   document.body.style.cursor = 'default';
 });
 question[2].addEventListener('click', function () {
-  if (answer[2].style.display === 'none') {
+  if (answer[2].style.display === 'block') {
+    answer[2].style.display = 'none';
+    question[2].style.fontWeight = 'normal';
+    arrowIcon[2].style.transform = 'rotate(0deg)'; 
+  } else {
     answer[2].style.display = 'block';
     question[2].style.fontWeight = 'bold';
     arrowIcon[2].style.transform = 'rotate(180deg)';
-  } else {
-    answer[2].style.display = 'none';
-    question[2].style.fontWeight = 'normal';
-    arrowIcon[2].style.transform = 'rotate(0deg)';
   }
 });
-
 
 question[3].addEventListener('mouseenter', function () {
   document.body.style.cursor = 'pointer';
@@ -66,17 +63,16 @@ question[3].addEventListener('mouseleave', function () {
   document.body.style.cursor = 'default';
 });
 question[3].addEventListener('click', function () {
-  if (answer[3].style.display === 'none') {
-    answer[3].style.display = 'block';
-    question[3].style.fontWeight = 'bold';
-    arrowIcon[3].style.transform = 'rotate(180deg)';
-  } else {
+  if (answer[3].style.display === 'block') {
     answer[3].style.display = 'none';
     question[3].style.fontWeight = 'normal';
     arrowIcon[3].style.transform = 'rotate(0deg)';
+  } else {
+    answer[3].style.display = 'block';
+    question[3].style.fontWeight = 'bold';
+    arrowIcon[3].style.transform = 'rotate(180deg)';
   }
 });
-
 
 question[4].addEventListener('mouseenter', function () {
   document.body.style.cursor = 'pointer';
@@ -85,14 +81,14 @@ question[4].addEventListener('mouseleave', function () {
   document.body.style.cursor = 'default';
 });
 question[4].addEventListener('click', function () {
-  if (answer[4].style.display === 'none') {
-    answer[4].style.display = 'block';
-    question[4].style.fontWeight = 'bold';
-    arrowIcon[4].style.transform = 'rotate(180deg)';
-  } else {
+  if (answer[4].style.display === 'block') {
     answer[4].style.display = 'none';
     question[4].style.fontWeight = 'normal';
     arrowIcon[4].style.transform = 'rotate(0deg)';
+  } else {
+    answer[4].style.display = 'block';
+    question[4].style.fontWeight = 'bold';
+    arrowIcon[4].style.transform = 'rotate(180deg)';
   }
 });
 
