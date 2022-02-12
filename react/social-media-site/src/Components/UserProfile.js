bdf0daa248907387233286738bcd13f3c3b8284a
Cole Sulak <nicole12sulak@gmail.com>
2022-02-12 14:06:23 -0600

    Added text to the triangles on the user profile page

--- react/social-media-site/src/Components/UserProfile.js
+++ react/social-media-site/src/Components/UserProfile.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
+import moment from "moment";
 import "../CSS/userprofile.css";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function UserProfile() {
-
+  let dob = moment("1996-04-30T19:26:49.610Z").utc().format('YYYY-MM-DD')
   const [user, setUser] = useState([])
 
   useEffect(() => {
@@ -20,77 +21,59 @@ function UserProfile() {
   }, []);
 
   return (
-     <div className="user-profile">
-      <Navbar />
+    <div>
+      <div className="user-profile">
+        <Navbar />
+        <section className="hex-container">
 
-    <div class="t">
+          <div className="t">
 
-      <div>
-        <div class="triangle">
-        {/* <p className="user-profile-name">{user.firstName} {user.lastName}</p> */}
-        </div>
+            <div>
+              <div className="triangle">
+                <p className="user-name">{user.firstName} {user.lastName}</p>
+              </div>
+            </div>
+
+            <div>
+              <div className="triangle">
+              <p className="user-dob">{dob}</p>
+              </div>
+            </div>
+
+            <div>
+              <div className="triangle">
+                <section className="astro-sign">Taurus</section>
+              </div>
+            </div>
+
+            <div>
+              <div className="triangle">
+              <section className="gender">{user.gender}</section>
+              </div>
+            </div>
+
+            <div>
+              <div className="triangle">
+              <section className="image-triangle">
+                  <img src={user.picture} alt=""/>
+                </section>
+              </div>
+            </div>
+
+            <div>
+              <div className="triangle">
+              <ul className="location">
+                {/* <li>{user.location.city},</li>
+                <li>{user.location.state},</li>
+                <li>{user.location.country}</li> */}
+              </ul> 
+
+              </div>
+            </div>
+            </div>
+        </section>
       </div>
-
-      <div>
-        <div class="triangle">
-
-        </div>
-      </div>
-
-      <div>
-        <div class="triangle">
-
-        </div>
-      </div>
-
-      <div>
-        <div class="triangle">
-
-        </div>
-      </div>
-      
-      <div>
-        <div class="triangle">
-
-        </div>
-      </div>
-      
-      <div>
-        <div class="triangle">
-
-        </div>
-      </div>
-
     </div>
-   
-
-    //   <div className="hex-container">
-
-    //   <section className="user-name-triangle">
-    //    
-    //   </section>
-
-    //     <section className="dob-triangle">
-    //       <p className="dob">{user.dateOfBirth}</p>
-    //     </section>
-
-    //     <section className="astro-triangle">
-    //       <p className="sign">astro sign</p>
-    //     </section>
-
-    //     <section className="gender-triangle">
-    //       <p className="gender">{user.gender}</p>
-    //     </section>
-
-    //     <section className="location-triangle">
-    //       {/* <p>{user.location.city}</p>
-    //       <p>{user.location.state}</p>
-    //       <p>{user.location.country}</p> */}
-
-    //     <img className="image-triangle" src="" alt="" />
-    //     </section>
-    //   </div>
-    // </div>
   );
 }
 
