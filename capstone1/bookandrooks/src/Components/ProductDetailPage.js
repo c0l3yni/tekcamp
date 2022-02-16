d3cd93ffc84a714c124ba50c8c22e5ad22a651a7
Cole Sulak <nicole12sulak@gmail.com>
2022-02-15 18:57:34 -0600

    Added temporary images to website, styled the product detail page, adjusted styling in nav and search bar

--- capstone1/bookandrooks/src/Components/ProductDetailPage.js
+++ capstone1/bookandrooks/src/Components/ProductDetailPage.js
@@ -1,30 +1,59 @@
 import react from "react";
+import "../CSS/productdetailpage.css";
 
 function ProductDetailPage() {
   return (
-    <div>
-      <section>
-        <img src="/" alt="" />
+    <div className="detail-box">
+      <section className="book-box">
+        <img
+          className="book-image"
+          src="https://pictures.depop.com/b0/5543036/839068852_c1e52c291997446c869fb88ae389bc6d/P0.jpg"
+          alt=""
+        />
       </section>
-      <section>
-        Book Title
-      </section>
-      <section>
-        Category: Romance/Young Adult
-      </section>
-      <section>
-        $45
-      </section>
-      <section>
-        Quanity: 3 books left
-      </section>
-      <section>
-        Serial: aknlkn32898
-      </section>
-      <section>
-        Manufactured by: Barnes and Nobles
-      </section>
-    
+      <div className="product-details">
+        <section>
+          <p>
+            <strong>Title:</strong> A Court of Thrones and Roses
+          </p>
+        </section>
+
+        <section>
+          <p>
+            <strong>Author:</strong> Sarah J. Maas
+          </p>
+        </section>
+
+        <section>
+          <p>
+            <strong>Category:</strong> Romance/Young Adult
+          </p>
+        </section>
+
+        <section>
+          <p>
+            <strong>Price:</strong> $45
+          </p>
+        </section>
+
+        <section>
+          <p>
+            <strong>Stock:</strong> 3 books left
+          </p>
+        </section>
+
+        <section>
+          <p>
+            <strong>Serial Number:</strong> 4CE0460D0G
+          </p>
+        </section>
+
+        <section>
+          <p>
+            <strong>Manufactured by:</strong> Edwards Brothers Malloy
+          </p>
+        </section>
+      </div>
     </div>
   );
 }
