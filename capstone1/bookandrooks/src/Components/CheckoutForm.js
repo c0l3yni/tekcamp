2116b4d3ef07c8e4dce143770bed58326ad18370
unknown <nicole12sulak@gmail.com>
2022-02-20 03:57:56 -0600

    Added error messages to form validation

--- capstone1/bookandrooks/src/Components/CheckoutForm.js
+++ capstone1/bookandrooks/src/Components/CheckoutForm.js
@@ -23,134 +23,122 @@ return (
             onChange={handleChange}
             {...errors.firstname && <div>{errors.firstname}</div>}
           />
-          {/* <p className="error-text" >First Name cannot be empty</p> */}
-          {/* <input
+          <p className="error-text" >First Name cannot be empty</p>
+          <input
             type="text"
             className="text-input"
+            name="lastname"
             placeholder="Last Name"
-            value={last}
-            onChange={(e) => setLast(e.target.value)}
+            value={values.lastname}
+            onChange={handleChange}
           ></input>
           <p className="error-text">Last Name cannot be empty</p>
         </div>
-
         <div className="input-row">
           <input
             type="text"
+            name="country"
             className="text-input"
             placeholder="Country"
-            value={country}
-            onChange={(e) => setCountry(e.target.value)}
+            value={values.country}
+            onChange={handleChange}
           ></input>
           <p className="error-text">Country cannot be empty</p>
-
           <input
             type="text"
+            name="country"
             className="text-input"
             placeholder="State"
-            value={states}
-            onChange={(e) => setStates(e.target.value)}
+            value={values.states}
+            onChange={handleChange}
           ></input>
           <p className="error-text">State cannot be empty</p>
         </div>
-
         <div className="input-row">
           <input
             type="text"
+            name="city"
             className="text-input"
             placeholder="City"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
+            value={values.city}
+            onChange={handleChange}
           ></input>
           <p className="error-text">City cannot be empty</p>
-
           <input
             type="text"
+            name="zip"
             className="text-input"
             placeholder="Zipcode"
-            value={zip}
-            onChange={(e) => setZip(e.target.value)}
+            value={values.zip}
+            onChange={handleChange}
           ></input>
           <p className="error-text">Zipcode cannot be empty</p>
         </div>
-
         <div className="input-row">
           <input
             type="text"
+            name="phone"
             className="text-input"
             placeholder="phone-number"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
+            value={values.phoneNum}
+            onChange={handleChange}
           ></input>
           <p className="error-text">Phone Number cannot be empty</p>
-
           <input
             type="text"
             className="text-input"
+            name="cardNum"
             placeholder="Card Number"
-            value={cardNum}
-            onChange={(e) => setCardNum(e.target.value)}
+            value={values.cardNum}
+            onChange={handleChange}
           ></input>
           <p className="error-text">Card Number cannot be empty</p>
         </div>
-
         <div className="input-row">
           <input
             type="text"
+            name="cardName"
             className="text-input"
             placeholder="Name on Card"
-            value={nameCard}
-            onChange={(e) => setNameCard(e.target.value)}
+            value={values.nameCard}
+            onChange={handleChange}
           ></input>
           <p className="error-text">Name on card cannot be empty</p>
-
           <input
             type="text"
+            name="month"
             className="text-input"
             placeholder="Expiration Month"
-            value={month}
-            onChange={(e) => setMonth(e.target.value)}
+            value={values.month}
+            onChange={handleChange}
           ></input>
           <p className="error-text">Card expiration month cannot be empty</p>
         </div>
-
         <div className="input-row">
           <input
             type="text"
+            name="year"
             className="text-input"
             placeholder="Expiration Year"
-            value={year}
-            onChange={(e) => setYear(e.target.value)}
-          ></input>
-          <p className="error-text">Card expiration year cannot be empty</p>
-
-          <input 
-          type="text" 
-          className="text-input" 
-          placeholder="CCV"
-          value={ccv}
-            onChange={(e) => setCCV(e.target.value)}
-          ></input>
-          <p className="error-text">Security code month cannot be empty</p>
-        </div> */}
-
-        <div className="input-row">
-          <input
-            type="email"
-            name="email"
-            className="text-input"
-            placeholder="Email Address"
-            value={values.email}
+            value={values.year}
             onChange={handleChange}
           ></input>
-            {errors.email && <p className="error">{errors.email}</p>}
+          <p className="error-text">Card expiration year cannot be empty</p>
+          <input 
+          type="text" 
+          name="ccv"
+          className="text-input" 
+          placeholder="CCV"
+          value={values.ccv}
+          onChange={handleChange}
+          ></input>
+          <p className="error-text">Security code month cannot be empty</p>
         </div>
   
         <div className="button-flex-box">
           <button className="form-submit" onClick={handleChange} type="button">
             Submit Purchase
           </button>
-        </div>
         </div>
       </form>
     </section>
--- capstone1/bookandrooks/src/Components/ValidateForm.js
+++ capstone1/bookandrooks/src/Components/ValidateForm.js
@@ -1,14 +1,52 @@
 export default function ValidateForm (values) {
   let errors = {}
 
-  if(values.firstname.trim()) {
+  if(values.firstname === '') {
     errors.firstname = "First Name is required"
   }
 
-  if(!values.email) {
-    errors.email = "Email is required"
-  } else if(!/^[A-Z0-9._%+-]A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-    errors.email = "Email is not valid"
+  if(values.lastname === '') {
+    errors.lastname = "Last Name is required"
+  }
+
+  if(values.country === '') {
+    errors.country = "Country is required"
+  }
+
+  if(values.city === '') {
+    errors.city = "City is required"
+  }
+
+  if(values.states === '') {
+    errors.states = "State is required"
+  }
+
+  if(values.phone.length < 10) {
+    errors.phone = "Phone is required"
+  }
+
+  if(values.zip.length < 5) {
+    errors.zip = "Zipcode is required"
+  }
+
+  if(values.cardNum.length < 16) {
+    errors.cardNum = "Card Number is required"
+  }
+
+  if(values.nameCard === '') {
+    errors.nameCard = "Name on Card is required"
+  }
+
+  if(values.month < 12) {
+    errors.month = "Card month is required"
+  }
+
+  if(values.year > 2017) {
+    errors.year = "Card year is required"
+  }
+
+  if(values.ccv.length == 3) {
+    errors.ccv = "Card CCV is required"
   }
 
 
