80c78918a6774e75cc7671f2e707ce79fc12bfcb
unknown <nicole12sulak@gmail.com>
2022-02-21 07:57:46 -0600

    navbar change

--- capstone1/bookandrooks/src/Components/Navbar.js
+++ capstone1/bookandrooks/src/Components/Navbar.js
@@ -4,17 +4,20 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
+
     <div className="nav-flex">
+         <Link className="link" to="/login">
+        <div className="flex-left">
+          <button className="button login">login</button>
+        </div>
+      </Link>
+      
       <Link className="link" to="/">
         <div className="title-box">
           <h1 className="title">Books and Rooks</h1>
         </div>
       </Link>
-      <Link className="link" to="/login">
-        <div className="flex-left">
-          <button className="button login">login</button>
-        </div>
-      </Link>
+   
       <div className="flex-right">
         <Link className="link" to="/cart">
           <button className="button cart">cart</button>
