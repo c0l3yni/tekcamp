b04c36a42800cb573d26edf8c3bcb0d633d01aa7
unknown <nicole12sulak@gmail.com>
2022-02-20 22:58:09 -0600

    Added staff view ability to edit products

--- capstone1/bookandrooks/src/Components/PrivateLogin.js
+++ capstone1/bookandrooks/src/Components/PrivateLogin.js
@@ -3,6 +3,7 @@ import ProductDetailAuthor from "./ProductDetailAuthor";
 import ProductDetailBook from "./ProductDetailBook";
 import ProductDetailCategory from "./ProductDetailCategory";
 import ProductDetailImage from "./ProductDetailImage";
+import ProductDetailSerialNum from "./ProductDetailSerialNum";
 import ProductDetailPrice from "./ProductDetailPrice";
 import ProductDetailQuantity from "./ProductDetailQuantity";
 import "../CSS/privatelogin.css";
@@ -14,7 +15,7 @@ function PrivateLogin({ products, setProducts }) {
     author: "",
     image: "",
     price: "",
-    quantity: "0",
+    quantity: "",
     qtyInCart: 0,
     serialNum: "",
     category: "",
@@ -35,6 +36,7 @@ function PrivateLogin({ products, setProducts }) {
               <ProductDetailBook product={product} />
               <ProductDetailAuthor product={product} />
               <ProductDetailQuantity product={product} />
+              <ProductDetailSerialNum product={product} />
             </div>
           </div>
           <br />
@@ -48,53 +50,123 @@ function PrivateLogin({ products, setProducts }) {
     setNewProduct({ ...newProduct, [event.target.name]: event.target.value });
   }
 
-  function handleAddProduct() {
+
+
+function searchInventory () {
+  const productExist = products?.products?.find((x) => x.serialNum === newProduct.serialNum);
+  console.log(productExist)
+  if (productExist) {
+    setNewProduct(productExist);
+    
+  }
+}
+
+function editInventory () {
+  const productExist = products?.products?.find((x) => x.serialNum === newProduct.serialNum);
+  if (productExist) {
+    let listWithoutBook={
+      products: {}
+    }
+     listWithoutBook.products = products?.products?.filter((x) => x.serialNum !== productExist.serialNum);
+    listWithoutBook.products.push(newProduct);
+    setProducts(listWithoutBook);
+    console.log(listWithoutBook)
+  } else {
     let prodList = products;
     setNewProduct({ ...newProduct, quantity: Number(newProduct.quantity) });
     prodList.products.push(newProduct);
     setProducts(prodList);
   }
-  
+
+/* 
+1. check to see if the new prod exists in inventory
+2. if exist filter the book out of list
+3. concat editted book to filtered list
+4. set products to concatted list
+*/
+
+}
+
+
   return (
     <div>
       <div className="admin-box">
         <h1 className="admin">Admin View</h1>
       </div>
       <div className="input-flex-box">
-        <h2 className="add-book">Add new book</h2>
+        <h2 className="add-book">Update Inventory</h2>
         <input
           className="input"
           onChange={handleInput}
-          name="book"
+          placeholder="Serial Number..."
+          name="serialNum"
+          value={newProduct.serialNum}
+        />
+        <input
+          className="input"
+          onChange={handleInput}
+          placeholder="Book index..."
+          name="index"
+          value={newProduct.index}
+        />
+        <input
+          className="input"
+          onChange={handleInput}
           placeholder="Book title..."
+          name="book"
+          value={newProduct.book}
         />
         <input
           className="input"
           onChange={handleInput}
           placeholder="Image url..."
           name="image"
+          value={newProduct.image}
         />
         <input
           className="input"
           onChange={handleInput}
           placeholder="Quantity in stock..."
           name="quantity"
+          value={newProduct.quantity}
         />
         <input
           className="input"
           onChange={handleInput}
           placeholder="Author..."
           name="author"
+          value={newProduct.author}
         />
         <input
           className="input"
           onChange={handleInput}
           placeholder="Price..."
           name="price"
+          value={newProduct.price}
         />
-        <button className="inventory-button" onClick={handleAddProduct}>
-          Add to inventory
+        
+        <input
+          className="input"
+          onChange={handleInput}
+          placeholder="Category..."
+          name="category"
+          value={newProduct.category}
+        />
+        <input
+          className="input"
+          onChange={handleInput}
+          placeholder="Manufacturer..."
+          name="manufacturer"
+          value={newProduct.manufacturer}
+        />
+        <div>
+        <button className="inventory-button" onClick={searchInventory}>
+          Search inventory
         </button>
+        <button className="inventory-button" onClick={editInventory}>
+          Update inventory
+        </button>
+        </div>
       </div>
       <div className="admin-box">
         <h2 className="inventory-title">Product Inventory</h2>
