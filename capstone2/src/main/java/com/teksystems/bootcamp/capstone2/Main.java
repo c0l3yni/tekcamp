019df60205a872fa3948b0e8e7e3290761860719
unknown <nicole12sulak@gmail.com>
2022-03-10 15:12:51 -0600

    Added UML diagram with classes described

--- capstone2/src/main/java/com/teksystems/bootcamp/capstone2/Main.java
+++ capstone2/src/main/java/com/teksystems/bootcamp/capstone2/Main.java
@@ -1,13 +1,10 @@
 package com.teksystems.bootcamp.capstone2;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class Main
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
     }
 }
